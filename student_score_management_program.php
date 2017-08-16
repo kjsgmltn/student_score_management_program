d93b84f5e339f93514498746dcfe1218218b460a
Lee <ehgjs1112@naver.com>
2017-08-16 18:39:32 +0900

    addfiles

--- student_score_management_program.php
+++ student_score_management_program.php
@@ -81,7 +81,7 @@ function subtractInputBox() {
             padding:5px 5px 5px 5px; margin:0px 0px 10px 0px;}	
 			
          #header_3 {
-            width:1200px; height:200px; background-color:; border:groove 1px silver;
+            width:1200px; height:200px; background-color:; border:groove 1px silver; overflow:scroll;
             padding:5px 5px 5px 5px; margin:0px 0px 10px 0px;}			
 
          #header_4 {
@@ -190,7 +190,7 @@ function subtractInputBox() {
 					</tr>
 					<?php
 						$i = 0;
-						$result = mysqli_query($conn,"select student_number from blueone.student_score_table;");
+						$result = mysqli_query($conn,"select student_number, student_name, student_language, student_english, student_science, student_society, student_class from blueone.student_score_table;");
 						if($conn){
 							while($text = mysqli_fetch_array($result)){
 								$number[$i] = $text['student_number'];
@@ -200,14 +200,17 @@ function subtractInputBox() {
 								$sci[$i] = $text['student_science'];
 								$soc[$i] = $text['student_society'];
 								$class[$i] = $text['student_class'];
-								$table_Str = "<tr>"."<td><input type="checkbox"name='CHECK' value =".$i."></td>".
-								"<td><output type="text" name='ST_NAME[]' size="10" maxlength="10">".$number[$i]."</td>"
-								<td><output type="text" name='ST_NAME[]' size="10" maxlength="10">".$name[$i]."</td>
-								<td><output type="text" name='ST_LANG_SCORE[]' size="3" maxlength="3">".$lang[$i]."</td>
-								<td><output type="text" name='ST_ENG_SCORE[]' size="3" maxlength="3">".$eng[$i]."</td>
-								<td><output type="text" name='ST_SCIEN_SCORE[]' size="3" maxlength="3">".$sci[$i].</td>
-								<td><output type="text" name='ST_SOCIE_SCORE[]' size="3" maxlength="3">".$soc[$i].</td>
-								<td><output type="text" name='ST_CLAS_NAME[]' size="1">".$class."</td></tr>";
+								$table_Str = "<tr>
+								<td><input type = checkbox name = CHECK></td>
+								<td><output type = text name = ST_NUMBER[] size = 10>".$number[$i]."</td>
+								<td><output type = text name = ST_NAME[] size = 10>".$name[$i]."</td>
+								<td><output type = text name = ST_LANG_SCORE[] size = 3>".$lang[$i]."</td>
+								<td><output type = text name = ST_ENG_SCORE[] size = 3>".$eng[$i]."</td>
+								<td><output type = text name = ST_SCIEN_SCORE[] size = 3>".$sci[$i]."</td>
+								<td><output type = text name = ST_SOCIE_SCORE[] size = 3>".$soc[$i]."</td>
+								<td><output type = text name = ST_CLAS_NAME[] size = 3>".$class[$i]."</td>
+								</tr>";
+								
 								echo $table_Str;
 								$i++;
 							}
